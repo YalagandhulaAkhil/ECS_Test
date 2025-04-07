1ceb15dceb6686d88974148cffd0736b05153551
YalagandhulaAkhil <131948225+YalagandhulaAkhil@users.noreply.github.com>
2025-04-07 16:53:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main" {
 # Public Subnet 1
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.10.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -28,7 +28,7 @@ resource "aws_subnet" "public_1" {
 # Public Subnet 2
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.20.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
