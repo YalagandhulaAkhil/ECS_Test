afe1e369867fa5fc375fe2ad1f5ba371b79a4d10
YalagandhulaAkhil <131948225+YalagandhulaAkhil@users.noreply.github.com>
2025-04-08 15:31:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,7 @@
 variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
-variable "container_image" {
-  description = "Docker image to deploy in ECS (e.g., nginx, your ECR image)"
-  type        = string
-  default     = "nginx"
+variable "app_name" {
+  default = "demo-app"
 }
