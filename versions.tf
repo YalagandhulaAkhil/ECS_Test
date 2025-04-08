44179c14edffc6f091178ba53a01985a9caec51f
YalagandhulaAkhil <131948225+YalagandhulaAkhil@users.noreply.github.com>
2025-04-08 15:30:58 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,22 @@
 terraform {
-  required_version = ">= 1.0.11"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+  required_version = ">= 1.3"
 
   backend "s3" {
-    bucket         = "terraform-state-files-159"
-    key            = "ecs/terraform.tfstate"
+    bucket         = "ecs-terraform-backend-ak"
+    key            = "ecs-deployment/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
